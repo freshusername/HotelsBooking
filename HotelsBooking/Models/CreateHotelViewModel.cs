601c0a968fd24b6922a23736c84d08ef4f9bdde4
serhiybodnarchuk <serhiybodnarchuk@gmail.com>
2019-12-10 00:14:36 +0200

    add responsive design for some pages

--- HotelsBooking/Models/CreateHotelViewModel.cs
+++ HotelsBooking/Models/CreateHotelViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using static Infrastructure.Enums;
@@ -9,8 +10,11 @@ namespace HotelsBooking.Models
     public class CreateHotelViewModel
     {
         public int Id { get; set; }
+        [Required]
         public string Name{ get; set; }
+        [Required]
         public string Location { get; set; }
+        [Required]
         public Season Season { get; set; }
     }
 }
