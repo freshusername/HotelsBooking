f2ef48317b6f2e25ff570150192275649058a5b8
dskopetskiy@gmail.com <dskopetskiy@gmail.com>
2019-12-12 14:32:41 +0200

    Home Page Initial Create, Home Controller

---
+++ HotelsBooking/Models/AppProfile/ProfileUpdateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelsBooking.Models.AppProfile
+{
+  public class ProfileUpdateViewModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+  }
+}
